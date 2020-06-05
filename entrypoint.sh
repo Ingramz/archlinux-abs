aa1627bbe44a1d92be9ac149190f17f438180be5
Indrek Ardel <indrek@ardel.eu>
2020-06-05 18:18:52 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-# `$*` expands the `args` supplied in an `array` individually
-# or splits `args` in a string separated by whitespace.
-sh -c "echo $*"
+su -l makepkg -c "echo $PRIVATE_KEY | gpg --import"
+su -l makepkg -c "echo $PUBLIC_KEY | gpg --import"
+pacman -Syu --nonconfirm
+mkdir /build
+cp -r * /build
+chown -R builder /build
+su -l makepkg -c 'cd /build && PACKAGER=$PACKAGER GPGKEY=$GPGKEY makepkg --sign --noconfirm -s'
+mkdir /repo
+mv /build/*.pkg.tar.xz /build/*.pkg.tar.xz.sign /repo
+cd /repo
+repo-add $REPONAME.db *.pkg.tar.xz
+mv /repo $GITHUB_WORKSPACE
