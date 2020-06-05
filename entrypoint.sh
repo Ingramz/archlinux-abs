1c7706c2f561d8610db98107d2e7541258645d81
Indrek Ardel <indrek@ardel.eu>
2020-06-05 18:39:06 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-su -l makepkg -c "echo $PRIVATE_KEY | gpg --import"
-su -l makepkg -c "echo $PUBLIC_KEY | gpg --import"
+su -l builder -c "echo $PRIVATE_KEY | gpg --import"
+su -l builder -c "echo $PUBLIC_KEY | gpg --import"
 pacman -Syu --nonconfirm
 mkdir /build
 cp -r * /build
 chown -R builder /build
-su -l makepkg -c 'cd /build && PACKAGER=$PACKAGER GPGKEY=$GPGKEY makepkg --sign --noconfirm -s'
+su -l builder -c 'cd /build && PACKAGER=$PACKAGER GPGKEY=$GPGKEY makepkg --sign --noconfirm -s'
 mkdir /repo
 mv /build/*.pkg.tar.xz /build/*.pkg.tar.xz.sign /repo
 cd /repo
